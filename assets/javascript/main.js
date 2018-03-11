8216eeb0019532820c040e170b0baace77ef5507
Jessica Perkins <jnperk1234@gmail.com>
2018-03-10 23:13:29 -0600

    upd1

---
+++ assets/javascript/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var topics = [];
+    function displayArcherChar(){
+        var x = $(this).data("search");
+        console.log(x);
+
+        var queryURL ="https://api.giphy.com/v1/gifs/search?q=" + x + "x7fJAVPi2MlTgl2ZvDZJiwA7gC6zYbsU"
+    }
+})
